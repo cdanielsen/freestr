c4f3f03cc6f50301b3f1f78d73745c6e26a1e90d
Christian Danielsen <ckdanielsen@gmail.com>
2015-12-05 15:50:59 -0800

    Add route for creating piles

--- routes/piles.js
+++ routes/piles.js
@@ -23,6 +23,26 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.post('/', function(req, res, next) {
+  var results = [];
+  var data = {name: req.body.name, location: req.body.location, items: req.body.number_of_items};
 
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      done();
+      console.log(err);
+    }
+    var query = client.query('INSERT INTO piles (name, location, number_of_items) VALUES ($1, $2, $3)', [data.name, data.location, data.items]);
+
+    var query = client.query('SELECT * FROM piles');
+    query.on('row', function(row) {
+      results.push(row);
+    });
+    query.on('end', function() {
+      done();
+      return res.json(results);
+    });
+  });
+});
 
 module.exports = router;
